a52202b53a1ea5740ea95bb34ac1ef69994d675d
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2025-09-02 01:16:07 +0530

    Debug login

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ passport.use(new LocalStrategy(
             }
             return done(null,user);
         }catch(err){
+            console.log(err);
         return done(err);
         }
     }
